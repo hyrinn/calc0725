81482b55171259519da880e09b7201b6d9f2a192
hyrinn <126777014+hyrinn@users.noreply.github.com>
2023-04-11 12:55:32 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name = 'hyorinn',
+    version = '0.2.5',
+    description = 'my calculator',
+    author = ' cho hyo rin'
+    url = 'https://github.com/hyrinn/calc0725',
+    download_url = 'https://github.com/hyrinn/calc0725/archive/refs/tags/v0.2.5.zip',
+    packages = ['mycalc'],
+    classfiers = [
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ]
+)
